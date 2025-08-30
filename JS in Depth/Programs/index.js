7c77beec25158015dcab2e671294168227b4082b
Shekhar629 <shekharkushwaha2018@gmail.com>
2025-08-30 23:49:39 +0530

    shekhar

--- JS in Depth/Programs/index.js
+++
@@ -1,5 +0,0 @@
-let x= { name:"shekhar",age:18};
-let y=x;
-let z={ name:"shekhar",age:18};
-
-console.log(x==z);
--- responsive/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle Dark/Light Mode
-const toggleSwitch = document.getElementById("modeToggle");
-const body = document.body;
-
-toggleSwitch.addEventListener("change", () => {
-  body.classList.toggle("dark");
-});
